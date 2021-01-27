72976f87e31612c8bb5e25912eb1ea5685ba5879
viewmendoza <viewmendoza@gmail.com>
2021-01-27 17:26:31 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# roboheart
+Created by Codrops
+
+Please read more about our license here: http://tympanus.net/codrops/licensing/ 
---
+++ fonts/robo_love211073/lte-ie7.js
@@ -0,0 +1,33 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'robo_love2\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-astroboy' : '&#x41;',
+			'icon-ironman' : '&#x42;',
+			'icon-mazinger' : '&#x43;',
+			'icon-robotech' : '&#x44;',
+			'icon-optimus_prime' : '&#x45;',
+			'icon-mechwarrior' : '&#x46;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
